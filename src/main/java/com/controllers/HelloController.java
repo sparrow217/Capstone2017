121997e55ba512e608ccf7b4c72d50c73304101c
jastein693 <jastein@unomaha.edu>
2017-09-28 22:09:32 -0500

    Ready to deploy

---
+++ src/main/java/com/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+
+public class HelloController {
+   @RequestMapping(value = "/hello", method = RequestMethod.GET)
+   public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "hello";
+   }
+}
