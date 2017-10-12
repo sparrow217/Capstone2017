c363b334c938c9879588b148840cafd1f410a886
jastein693 <jastein@unomaha.edu>
2017-10-11 23:00:12 -0500

    Added index servlet

--- src/main/java/com/capstone/Main.java
+++ src/main/java/com/capstone/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	 public static void main(String[] args) throws Exception {
 		 String webappDirLocation = "src/main/webapp/";
 	        Tomcat tomcat = new Tomcat();
-	        tomcat.setBaseDir("/target");
+	        
 	        
 	        //The port that we should run on can be set into an environment variable
 	        //Look for that variable and default to 8080 if it isn't there.
